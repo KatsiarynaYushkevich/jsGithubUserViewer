3a0001d73706179c8201557c64f9fd6c599f8933
KatsiarynaYushkevich <e.yushkevich30@gmail.com>
2025-02-26 16:52:51 +0300

    added 2 task

--- js/app.js
+++ js/app.js
@@ -15,25 +15,45 @@ function addUserInfo(userInfo) {
   `;
 }
 
+// function showUsersInfo(users) {
+//   users.forEach((el) => {
+//     user.innerHTML += `
+//      <p>${el.name}</p>
+//   `
+//   });
+// }
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  const getUser = async () => {
-    try {
-      const response = await fetch(
-        `https://api.github.com/users/${userInput.value}`
-      );
-      if (!response.ok) {
-        throw new Error("Ошибка статус-кода");
-      }
-
-      const userInfo = await response.json();
-      addUserInfo(userInfo);
-    } catch (error) {
-      console.error(error);
-    }
-  };
   getUser();
-
+  // getJsonplaceholderUsers();
   userInput.value = "";
 });
+
+const getUser = async () => {
+  try {
+    const response = await fetch(
+      `https://api.github.com/users/${userInput.value}`
+    );
+    if (!response.ok) {
+      throw new Error("Ошибка статус-кода");
+    }
+    const userInfo = await response.json();
+    addUserInfo(userInfo);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// const getJsonplaceholderUsers = async () => {
+//   try {
+//     const users = await fetch(`https://jsonplaceholder.typicode.com/users`);
+//     if (!users.ok) {
+//       throw new Error("Ошибка статус-кода");
+//     }
+//     const jsonplaceholderUsers = await users.json();
+//     showUsersInfo(jsonplaceholderUsers);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
